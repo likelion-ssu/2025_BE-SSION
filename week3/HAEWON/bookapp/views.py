e1fb6f41b2603333a2b7de413c13c8167a143c01
chohaewon48 <chocynthia@naver.com>
2025-05-26 19:07:10 +0900

    3주차 과제

---
+++ week3/HAEWON/bookapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from bookapp.models import Book
+
+# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    print(books.values())
+    data = {
+        'books' : list(books.values())
+    }
+    return JsonResponse(data)
+
+def book_details(request, id) : 
+    book = Book.objects.get(id=id)
+    data = {
+        'title' : book.title,
+        'author' : book.author,
+        'isbn' : book.isbn
+    }
+    return JsonResponse(data)
